45cd27dfdb6d738e26abe481cb2da54e8c49ecff
raghavgarg098 <raghavgarg98@gmail.com>
2023-08-06 20:28:22 +0530

    Adding the frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './Helloworld';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img  className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <HelloWorld />
+      </header>
+    </div>
+  );
+}
+
+export default App;
